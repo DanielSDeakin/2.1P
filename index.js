ad6d8fc424dcaf8939bedc9c024d168ed9df46a1
Daniel <s222274057@deakin.edu.au>
2025-08-01 16:30:48 +1000

    test email send

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello world!')
+﻿require('dotenv').config();
+const sendGrid = require("@sendgrid/mail");
+sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function sendEmail() {
+    const messageData = {
+        to: 'dsarson2@gmail.com',
+        from: 'danielsarson16@gmail.com',
+        subject: 'test',
+        text: 'test',
+        html: '<p>test</p>'
+    }
+    try {
+        await sendGrid.send(messageData);
+        console.log('message sent');
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+sendEmail();
