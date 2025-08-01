9333064b26e500195555f6b362ddf0fd6303c29b
Daniel <s222274057@deakin.edu.au>
2025-08-01 19:42:28 +1000

    final changes, linking

---
+++ server.js
@@ -0,0 +1,21 @@
+﻿require('dotenv').config();
+const express = require('express');
+const sendGrid = require('@sendgrid/mail');
+
+sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/subscribe', async (req, res) => {
+    sendGrid.send({
+        to: req.body.email,
+        from: 'danielsarson16@gmail.com',
+        subject: 'Subscription',
+        text: 'Thanks for subscribing!',
+    });
+    res.send('Email sent');
+});
+
+app.listen(3000);
